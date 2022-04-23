b95a69c63e21e21932f1cef58c623511e5505639
Just1truc <justin.duc@epitech.eu>
2022-04-23 19:36:55 +0200

    [UPDATE] Fixing main problems

--- main.py
+++ main.py
@@ -1,81 +1,62 @@
-
+import sys
 import email
 import re
 from urllib import response
 from requests import request, session
 import time
-# Importing
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 from bs4 import BeautifulSoup
 
-def connectUser():
-    error = True
-
-    driver = []
-
-    while (error):
-        print("Please enter your informations :")
-        print("Email Or Phone number: ", end="")
-        email_ = input()
-        print("Password: ", end="")
-        password_ = input()
-
-        print("Loading web page...")
-
-        driver = webdriver.Chrome()
-        driver.get('https://www.linkedin.com/uas/login')
-        form = driver.find_element(by=By.CLASS_NAME, value="login__form")
-        email_input = form.find_element(by=By.ID, value="username")
-        password_input = form.find_element(by=By.ID, value="password")
-
-        email_input.send_keys(email_)
-        password_input.send_keys(password_)
-        form.submit()
-
-        post_response = BeautifulSoup(driver.page_source, features='lxml')
-        workin = False
-        error_username = ""
-        error_password = ""
-        while workin == False and driver.current_url != 'https://www.linkedin.com/feed/':
-            if (post_response.find(id='error-for-username') != None and post_response.find(id='error-for-password') != None):
-                error_username = str(post_response.find(id='error-for-username')).split('>')[1].split('<')[0]
-                error_password = str(post_response.find(id='error-for-password')).split('>')[1].split('<')[0]
-                workin = True
-            else:
-                time.sleep(1)
-                post_response = BeautifulSoup(driver.page_source, features='lxml')
-
-
-        if driver.current_url == 'https://www.linkedin.com/feed/':
-            break
-
-        print("An error occured. The credential must be invalids")
-        print("The error is : " + error_username + error_password + "\n")
-        driver.close()
-
-
-    print('Connected !\n')
-    return driver
+from sources.connectUser import connectUser
+sys.path.insert(1,'./sources/')
+import connectUser
 
 # Main
 if __name__ == '__main__':
     print("Starting...")
-    driver = connectUser()
+    driver = connectUser.connectUser()
 
-    print("Please Enter the word you want to use to scrap peoples profiles: ", end="")
-    word = input()
-    print("Please enter the speed of scrapping (In seconds between each scrap to not seem sus): ", end="")
-    speed = int(input())
-    print("Please Enter the number of profile you want to get", end="")
-    number_of_profile = int(input())
+    ##print("Please Enter the word you want to use to scrap peoples profiles: ", end="")
+    ##word = input()
+    ##print("Please enter the speed of scrapping (In seconds between each scrap to not seem sus): ", end="")
+    ##speed = int(input())
+    ##print("Please Enter the number of profile you want to get: ", end="")
+    ##number_of_profile = int(input())
+
+    print("Getting to connections...")
 
     profile_div = driver.find_element(by=By.CLASS_NAME, value="feed-identity-module__actor-meta")
     link = profile_div.find_element(by=By.CLASS_NAME, value="ember-view")
     link.click()
-    print(driver.current_url)
 
+    time.sleep(2)
+
+    profile_div = driver.find_element(by=By.CLASS_NAME, value="ph5")
+    links = profile_div.find_elements(by=By.TAG_NAME, value="a")
+    for elem in links:
+        if (elem.get_attribute("href") == "https://www.linkedin.com/search/results/people/?network=%5B%22F%22%5D&origin=MEMBER_PROFILE_CANNED_SEARCH"):
+            elem.click()
+            break
+
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0, 2000)")
+    time.sleep(2)
+    workdiv = driver.find_element(by=By.CLASS_NAME, value="search-results-container")
+    workdiv = workdiv.find_elements(by=By.TAG_NAME, value="button")
+    for i in workdiv:
+        print(i.get_attribute("aria-label"))
+    time.sleep(5)
+
+    items = driver.find_elements(by=By.CLASS_NAME, value="entity-result__item")
+    for element in items:
+        subtitle = element.find_element(by=By.CLASS_NAME, value="entity-result__primary-subtitle")
+        print(subtitle.get_attribute("innerHTML").replace("<!---->","").replace("        ", "").replace("\n", ""))
+
+    driver.refresh()
+    time.sleep(2)
 
     
 
---
+++ sources/connectUser.py
@@ -0,0 +1,73 @@
+import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import json
+import time
+
+def getCredentials(path):
+    buffer = open(path, "r")
+    inside = buffer.read()
+    dict = json.loads(inside)
+    if "username" in dict and "password" in dict:
+        return dict["username"], dict["password"]
+    return False, False
+
+def connectUser():
+    error = True
+
+    driver = []
+    i = 0
+
+    while (error):
+        print("Please enter your informations :")
+        if i==0:
+            email_, password_ = getCredentials("./credentials.json")
+            if email_ == False and password_ == False:
+                print("Email Or Phone number: ", end="")
+                email_ = input()
+                print("Password: ", end="")
+                password_ = input()
+        else:
+            print("Email Or Phone number: ", end="")
+            email_ = input()
+            print("Password: ", end="")
+            password_ = input()
+
+        print("Loading web page...")
+
+        driver = webdriver.Chrome()
+        driver.get('https://www.linkedin.com/uas/login')
+        form = driver.find_element(by=By.CLASS_NAME, value="login__form")
+        email_input = form.find_element(by=By.ID, value="username")
+        password_input = form.find_element(by=By.ID, value="password")
+
+        email_input.send_keys(email_)
+        password_input.send_keys(password_)
+        form.submit()
+
+        post_response = BeautifulSoup(driver.page_source, features='lxml')
+        workin = False
+        error_username = ""
+        error_password = ""
+        while workin == False and driver.current_url != 'https://www.linkedin.com/feed/':
+            if (post_response.find(id='error-for-username') != None and post_response.find(id='error-for-password') != None):
+                error_username = str(post_response.find(id='error-for-username')).split('>')[1].split('<')[0]
+                error_password = str(post_response.find(id='error-for-password')).split('>')[1].split('<')[0]
+                workin = True
+            else:
+                time.sleep(1)
+                post_response = BeautifulSoup(driver.page_source, features='lxml')
+
+
+        if driver.current_url == 'https://www.linkedin.com/feed/':
+            break
+
+        print("An error occured. The credential must be invalids")
+        print("The error is : " + error_username + error_password + "\n")
+        driver.close()
+        i += 1
+
+
+    print('Connected !\n')
+    return driver
